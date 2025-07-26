5df08293d7fcf30ec111eb57953574cb5b88fcd3
Guilherme Cavalcante <168848497+Cavalcante-g@users.noreply.github.com>
2025-07-25 23:54:16 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx"
 import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import PokemonPage from "./pages/PokemonPage.jsx"
-import './index.css'
+import './app.css'
 
 const router = createBrowserRouter([
   {
